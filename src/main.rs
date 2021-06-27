56f3ae8a60085983e43f67ab6966e9dcad8d1b2a
Floozutter <me@floozutter.site>
2021-06-27 02:40:50 -0700

    refactor with prompt_midi

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-use midir::MidiInput;
+use midir::{MidiInput, MidiInputPort};
 use buttplug::{
     client::{
-        ButtplugClient, ButtplugClientDevice, ButtplugClientDeviceMessageType, ButtplugClientEvent,
+        ButtplugClient, ButtplugClientEvent, ButtplugClientDeviceMessageType, 
         VibrateCommand,
     },
     server::ButtplugServerOptions,
@@ -11,41 +11,50 @@ use tokio::io::{self, AsyncBufReadExt, BufReader};
 use tokio::runtime::Handle;
 use futures::StreamExt;
 use futures_timer::Delay;
-use std::{sync::Arc, time::Duration, io::Write};
-async fn run() {
-    // select MIDI input port
-    let imidi = MidiInput::new("buzzkey midir input").unwrap();
-    let iports = imidi.ports();
-    let iport = match iports.len() {
-        0 => {
-            println!("no available MIDI input port found!");
-            return;
-        },
+use std::{error::Error, time::Duration, io::Write};
+
+fn prompt_midi(
+    client_name: &str
+) -> Result<(MidiInput, MidiInputPort), Box<dyn Error>> {
+    let imidi = MidiInput::new(client_name)?;
+    let mut iports = imidi.ports();
+    match iports.len() {
+        0 => Err("no available MIDI input port found".into()),
         1 => {
-            println!("only available MIDI input port: {}", imidi.port_name(&iports[0]).unwrap());
-            &iports[0]
+            println!(
+                "selecting only available MIDI input port: {}",
+                imidi.port_name(&iports[0])?
+            );
+            Ok((imidi, iports.pop().unwrap()))
         },
         _ => {
-            println!("available MIDI input ports:");
+            println!("available input ports:");
             for (i, p) in iports.iter().enumerate() {
-                println!("{}: {}", i, imidi.port_name(p).unwrap());
+                println!("{}: {}", i, imidi.port_name(p)?);
             }
-            print!("select MIDI input port: ");
-            std::io::stdout().flush().unwrap();
+            print!("select input port: ");
+            std::io::stdout().flush()?;
             let mut input = String::new();
-            std::io::stdin().read_line(&mut input).unwrap();
-            iports.get(input.trim().parse::<usize>().unwrap()).unwrap()
-        }
-    };
+            std::io::stdin().read_line(&mut input)?;
+            Ok((
+                imidi,
+                iports.into_iter().nth(
+                    input.trim().parse::<usize>()?
+                ).ok_or("invalid input port selected")?
+            ))
+        },
+    }
+}
+
+async fn run() -> Result<(), Box<dyn Error>> {
+    // select MIDI input port
+    let (imidi, iport) = prompt_midi("buzzkey midir input")?;
     // connect Buttplug
     let client = ButtplugClient::new("buzzkey buttplug client");
     let mut event_stream = client.event_stream();
-    client.connect_in_process(&ButtplugServerOptions::default()).await.unwrap();
+    client.connect_in_process(&ButtplugServerOptions::default()).await?;
     // scan for devices
-    if let Err(err) = client.start_scanning().await {
-        println!("buzzkey client failed to start scan!: {}", err);
-        return;
-    }
+    client.start_scanning().await?;
     async_manager::spawn(async move {
         loop {
             match event_stream.next().await.unwrap() {
@@ -53,23 +62,23 @@ async fn run() {
                     async_manager::spawn(async move {
                         println!("device added: {}", dev.name);
                     }).unwrap();
-                }
+                },
                 ButtplugClientEvent::ScanningFinished => (),
                 ButtplugClientEvent::ServerDisconnect => {
                     println!("server disconnected!");
-                }
+                },
                 _ => {
                     println!("something happened!");
-                }
+                },
             }
         }
-    }).unwrap();
+    })?;
     println!("scanning started! press enter at any point to stop and start buzzing.");
-    BufReader::new(io::stdin()).lines().next_line().await.unwrap();
-    client.stop_scanning().await.unwrap();
+    BufReader::new(io::stdin()).lines().next_line().await?;
+    client.stop_scanning().await?;
     // buzz on MIDI input
     let handle = Handle::current();
-    let _iport_connection = imidi.connect(iport, "midi-chaos_iport", move |stamp, message, _| {
+    let _iport_connection = imidi.connect(&iport, "buzzkey_iport", move |stamp, message, _| {
         println!("{}: {:?} (len = {})", stamp, message, message.len());
         let status = message[0];
         println!("{:b}", status >> 4);
@@ -88,13 +97,16 @@ async fn run() {
                 });
             }
         }
-    }, ()).unwrap();
+    }, ())?;
     println!("buzzing started! press enter at any point to quit.");
-    BufReader::new(io::stdin()).lines().next_line().await.unwrap();
+    BufReader::new(io::stdin()).lines().next_line().await?;
     println!("bye-bye! >:3c");
+    Ok(())
 }
 
 #[tokio::main]
 async fn main() {
-    run().await;
+    if let Err(err) = run().await {
+        eprintln!("error: {}!", err);
+    };
 }
